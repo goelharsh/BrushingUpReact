2651f660e6cc12e13095c34fd751984f133152a6
goelharsh <harshgoel18122001@gmail.com>
2024-04-27 16:51:04 +0530

    0102custom

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>classic react app</div>
+  )
+}
+
+export default App
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Youtube from './Youtube'
+
+const App = () => {
+  return (
+    <>
+      <h1>Vite React App</h1>
+      <Youtube/>
+    </>
+   
+  )
+}
+
+export default App
